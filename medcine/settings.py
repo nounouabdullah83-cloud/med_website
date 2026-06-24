9b2eb3a31329996e5dda11a5d9cbfe5609cd8377
nounouabdullah83-cloud <nounouabdullah83@gmail.com>
2026-06-24 20:02:37 +0200

    Database URL support

--- medcine/settings.py
+++ medcine/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,14 +84,9 @@ WSGI_APPLICATION = 'medcine.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medicine_db',      # أو اسم قاعدة بياناتك
-        'USER': 'postgres',
-        'PASSWORD': 'NewStrongPassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
