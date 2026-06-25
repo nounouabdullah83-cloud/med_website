5e4a7dda65eb6add40a5c58017c03b88ad08de2b
nounouabdullah83-cloud <nounouabdullah83@gmail.com>
2026-06-25 15:34:13 +0200

    fix the media issue

--- medcine/urls.py
+++ medcine/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
     path('statistics/', include('statictics.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
+
+urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
