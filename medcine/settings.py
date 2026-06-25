fe976ab841f4518c1fc304c18b3dcc08e233cc02
nounouabdullah83-cloud <nounouabdullah83@gmail.com>
2026-06-25 14:55:59 +0200

    fix static

--- medcine/settings.py
+++ medcine/settings.py
@@ -136,7 +136,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
